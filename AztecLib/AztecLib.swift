eb0c9be36008b3ce693eaad247d34b801ad840d6
Toni Sucic <toni.sucic@icloud.com>
2025-10-10 10:38:40 +0200

    Initial Commit

---
+++ AztecLib/AztecLib.swift
@@ -0,0 +1,10 @@
+//
+//  AztecLib.swift
+//  AztecLib
+//
+//  Created by Toni Sucic on 10/10/2025.
+//
+
+class AztecLib {
+
+}
---
+++ AztecLibTests/AztecLibTests.swift
@@ -0,0 +1,17 @@
+//
+//  AztecLibTests.swift
+//  AztecLibTests
+//
+//  Created by Toni Sucic on 10/10/2025.
+//
+
+import Testing
+@testable import AztecLib
+
+struct AztecLibTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
